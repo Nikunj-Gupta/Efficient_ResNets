f7d729f53cfb91370e242d573a7fdb60ce69ea7a
Nikunj Gupta <ng2531@nyu.edu>
2022-03-07 01:26:21 -0500

    runs

---
+++ run_utils.py
@@ -0,0 +1,70 @@
+import os 
+from pathlib import Path 
+from itertools import count
+
+dumpdir = "runs/" 
+if not os.path.isdir(dumpdir):
+    os.mkdir(dumpdir)
+fixed_text = "#!/bin/bash\n"\
+             "#SBATCH --nodes=1\n"\
+             "#SBATCH --cpus-per-task=16 \n"\
+             "#SBATCH --time=1:00:00\n"\
+             "#SBATCH --mem=40GB\n"\
+             "#SBATCH --gres=gpu:1\n"
+
+for opt in ['sgd', 'adam']: 
+    if opt=='sgd': 
+
+        for lr_sched in ['CosineAnnealingLR','LambdaLR', 'MultiplicativeLR',  
+                            'StepLR',  'MultiStepLR',  'ExponentialLR', 
+                             'CyclicLR',  'CyclicLR2',  'CyclicLR3', 
+                             'OneCycleLR',  'OneCycleLR2',  'CosineAnnealingWarmRestarts']: 
+            exp = os.path.join('optimizers', '_'.join([opt, lr_sched])) 
+            command = fixed_text + "#SBATCH --job-name="+exp+"\n" + "#SBATCH --output="+exp+".out\n"
+            command += "\nmodule load python/intel/3.8.6\n"\
+                        "module load openmpi/intel/4.0.5\n"\
+                        "\nsource ../venvs/dl/bin/activate\n"\
+                        "time python3 main.py " 
+            
+            command = ' '.join([
+                command, 
+                "--exp", exp, 
+                "--opt", opt, 
+                "--lr_sched", lr_sched 
+            ])
+
+            # print(command) 
+            log_dir = Path(dumpdir)
+            for i in count(0):
+                temp = log_dir/('run{}.sh'.format(i)) 
+                if temp.exists():
+                    pass
+                else:
+                    with open(temp, "w") as f:
+                        f.write(command) 
+                    log_dir = temp
+                    break
+    elif opt=='adam': 
+        exp = os.path.join('optimizers', opt) 
+        command = fixed_text + "#SBATCH --job-name="+exp+"\n" + "#SBATCH --output="+exp+".out\n"
+        command += "\nmodule load python/intel/3.8.6\n"\
+                    "module load openmpi/intel/4.0.5\n"\
+                    "\nsource ../venvs/dl/bin/activate\n"\
+                    "time python3 main.py " 
+        
+        command = ' '.join([
+            command, 
+            "--exp", exp, 
+            "--opt", opt, 
+        ])
+        # print(command) 
+        log_dir = Path(dumpdir)
+        for i in count(0):
+            temp = log_dir/('run{}.sh'.format(i)) 
+            if temp.exists():
+                pass
+            else:
+                with open(temp, "w") as f:
+                    f.write(command) 
+                log_dir = temp
+                break 
---
+++ runs/run5.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=16 
+#SBATCH --time=1:00:00
+#SBATCH --mem=40GB
+#SBATCH --gres=gpu:1
+#SBATCH --job-name=optimizers/sgd_ExponentialLR
+#SBATCH --output=optimizers/sgd_ExponentialLR.out
+
+module load python/intel/3.8.6
+module load openmpi/intel/4.0.5
+
+source ../venvs/dl/bin/activate
+time python3 main.py  --exp optimizers/sgd_ExponentialLR --opt sgd --lr_sched ExponentialLR
