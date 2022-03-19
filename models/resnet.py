aad69bc0ed7bdae6bb39eaa6d4602072370fafb4
Nikunj Gupta <ng2531@nyu.edu>
2022-03-19 18:26:44 -0400

    added dropout layer; cleaned up get_ResNet()

--- models/resnet.py
+++ models/resnet.py
@@ -74,18 +74,17 @@ class Bottleneck(nn.Module):
 
 class ResNet(nn.Module):
     def __init__(
-        self, 
-        block, 
-        num_blocks, 
-        conv_kernel_sizes=None, 
-        shortcut_kernel_sizes=None,
-        num_classes=10, 
-        num_channels=64, 
-        avg_pool_kernel_size=4):
-
-
+            self, 
+            block, 
+            num_blocks, 
+            conv_kernel_sizes=None, 
+            shortcut_kernel_sizes=None,
+            num_classes=10, 
+            num_channels=64, 
+            avg_pool_kernel_size=4, 
+            drop=None):
         super(ResNet, self).__init__()
-        self.in_planes = 64 
+        self.in_planes = num_channels
         self.avg_pool_kernel_size = avg_pool_kernel_size 
         """
         # of channels Ci 
@@ -93,6 +92,7 @@ class ResNet(nn.Module):
         self.num_channels = num_channels 
         self.conv1 = nn.Conv2d(3, self.num_channels, kernel_size=3,stride=1, padding=1, bias=False)
         self.bn1 = nn.BatchNorm2d(self.num_channels)
+        self.drop = drop 
         """
         # of Residual Layers N 
         # of Residual Blocks Bi 
@@ -111,7 +111,12 @@ class ResNet(nn.Module):
                                                     shortcut_kernel_size=shortcut_kernel_size)) 
 
         self.residual_layers = nn.ModuleList(self.residual_layers)
-        self.linear = nn.Linear(512*block.expansion, num_classes)
+        self.linear = nn.Linear(512*block.expansion, num_classes) 
+        """
+        Dropout layer 
+        """
+        if self.drop: 
+            self.dropout = nn.Dropout(self.drop) # Define proportion or neurons to dropout
 
     def _make_layer(self, block, planes, num_blocks, stride, conv_kernel_size, shortcut_kernel_size):
         strides = [stride] + [1]*(num_blocks-1)
@@ -130,18 +135,40 @@ class ResNet(nn.Module):
         """
         out = F.avg_pool2d(out, self.avg_pool_kernel_size)
         out = out.view(out.size(0), -1)
+        if self.drop: out = self.dropout(out)
         out = self.linear(out)
         return out
 
-def ResNet_new():
+def get_ResNet(config): 
+    net =  ResNet(
+            block=BasicBlock, 
+            num_blocks=config['num_blocks'],                          # N: number of Residual Layers | Bi:Residual blocks in Residual Layer i 
+            conv_kernel_sizes=config['conv_kernel_sizes'],            # Fi: Conv. kernel size in Residual Layer i 
+            shortcut_kernel_sizes=config['shortcut_kernel_sizes'],    # Ki: Skip connection kernel size in Residual Layer i 
+            num_channels=config['num_channels'],                      # Ci: # channels in Residual Layer i 
+            avg_pool_kernel_size=config['avg_pool_kernel_size'],      # P: Average pool kernel size 
+            drop=config['drop']                                       # use dropout with drop proportion 
+        ) 
+    
+    total_params = 0 
+    for x in filter(lambda p: p.requires_grad, net.parameters()):
+        total_params += np.prod(x.data.numpy().shape)
+    # print("Total number of params", total_params)
+    # print("Total layers", len(list(filter(lambda p: p.requires_grad and len(p.data.size())>1, net.parameters())))) 
+
+    return net, total_params 
+
+
+def get_ResNet_default():
     return ResNet(
         block=BasicBlock, 
         num_blocks=[2,1,1,1],               # N: number of Residual Layers | Bi:Residual blocks in Residual Layer i 
-        conv_kernel_sizes=[2,1,1,1],        # Fi: Conv. kernel size in Residual Layer i 
-        shortcut_kernel_sizes=[2,1,1,1],    # Ki: Skip connection kernel size in Residual Layer i 
+        conv_kernel_sizes=[3,3,3,3],        # Fi: Conv. kernel size in Residual Layer i 
+        shortcut_kernel_sizes=[1,1,1,1],    # Ki: Skip connection kernel size in Residual Layer i 
         num_channels=64,                    # Ci: # channels in Residual Layer i
         avg_pool_kernel_size=4              # P: Average pool kernel size 
     ) 
+
 
 def ResNet9():
     return ResNet(BasicBlock, [2, 1, 1, 1])
@@ -172,7 +199,7 @@ def test():
     print(y.size())
 
 def test2():
-    net = ResNet_new()
+    net = get_ResNet_default()
     total_params = 0 
     print(net)
 
@@ -181,5 +208,5 @@ def test2():
     print("Total number of params", total_params)
     print("Total layers", len(list(filter(lambda p: p.requires_grad and len(p.data.size())>1, net.parameters()))))
 
-test() 
-test2() 
+# test() 
+# test2() 
